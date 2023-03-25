eb8ff454ba1b5771afef060805df41a694a1ab96
ADIB-the-NOOB <mdadib550@gmail.com>
2023-03-26 01:11:15 +0600

    Return statement

--- C# Basic to Advanced/Program.cs
+++ C# Basic to Advanced/Program.cs
@@ -1,40 +1,25 @@
 ﻿using System;
-using System.Runtime.Serialization;
 // System all the collections of classes like - Console
 // This namespace is a collection of - Interfaces, Structs, enum and Delegates
 
 class Program
 {
-    static void Main()
-    // Main() method is the entry point of the Program.
+    static void Main(string[] args)
     {
-
-        /*
-            string[] carList = new string[3];
-            carList[0] = "VN";
-            carList[1] = "F";
-            carList[2] = "C";
-            Console.WriteLine(carList[1]);
-        */
-        /* Console.WriteLine("please give me your Number:");
-        int userNumber = int.Parse(Console.ReadLine());
-        Console.WriteLine(userNumber);
-        Console.WriteLine();
-        */
-
-        Console.WriteLine("phone_number");
-        int userNumber = int.Parse(Console.ReadLine()); 
-
-        switch(userNumber)
-        {
-            case 10:
-                Console.WriteLine("Number is 10.");
-                break;
-            case 20:
-                Console.WriteLine("Number is 20");
-                break;
-            default: Console.WriteLine("Number is not 10 or 20"); break;
-        }
-
+        sayHi("adib", 12, "CZS");
+        Console.WriteLine(cube(2));
     }
+    static void sayHi(string name, int age, string school)
+    {
+        Console.WriteLine("Hello " + name);
+        Console.WriteLine("Your age is " + age);
+        Console.WriteLine("your school is " + school);
+    } 
+        
+    static int cube(int num)
+    {
+        int result = num * num * num;
+        return result;
+    }
+
 }
